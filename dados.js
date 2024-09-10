4adfce84c3e45a62bcff4525477f8478b1c96fb9
Redfield1302 <jairccarmo@gmail.com>
2024-09-09 23:16:15 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Diego Hypólito",
+        descricao: "Diego Hypólito é um ginasta brasileiro, conhecido por sua habilidade nas argolas e por sua trajetória marcada por superação e determinação. Foi medalhista olímpico e mundial.",
+        link: "https://pt.wikipedia.org/wiki/Diego_Hyp%C3%B3lito",
+        tags: "ginastasta ouro brasileira"
+    },   
+    {
+        titulo: "Daiane dos Santos",
+        descricao: "Daiane dos Santos é uma ginasta brasileira, conhecida por sua habilidade no salto sobre a mesa e por ser a primeira ginasta brasileira a conquistar uma medalha de ouro em um Mundial de Ginástica Artística.",
+        link: "https://pt.wikipedia.org/wiki/Daiane_dos_Santos",
+        tags: "ginastasta"
+        
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Diego_Hyp%C3%B3lito",
+        tags: "ginastasta ouro brasileira"
+    },
+];
